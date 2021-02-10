fa153d211db0abd0067b73b0a794bb43cd8e6610
Kieran Gallacher <2463778g@student.gla.ac.uk>
2021-02-10 16:12:40 +0000

    update 17

--- populate_rango.py
+++ populate_rango.py
@@ -32,9 +32,9 @@ def populate():
          'url':'http://flask.pocoo.org'}]
     
     
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
     
     
     
@@ -56,8 +56,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, likes=0, views=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c = Category.objects.get_or_create(likes=likes)[0]
+    c = Category.objects.get_or_create(views=views)[0]
     c.save()
     return c
     
